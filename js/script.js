2ceabe3d51bcf4d7b6f731c6621e421ef2639f10
Luis Felipe Barbosa Petrillo <luisfelipebarbosa2013@gmail.com>
2022-07-21 11:29:33 -0300

    TabNav transformado em classe

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import initAnimacaoScroll from "./modules/scroll-animacao.js";
-import initAccordion from "./modules/accordion.js";
-import initTabNav from "./modules/tabnav.js";
+import Accordion from "./modules/accordion.js";
+import TabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
@@ -9,13 +8,20 @@ import initMenuMobile from "./modules/menu-mobile.js";
 import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import initAnimacaoScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-initAnimacaoScroll();
-initAccordion();
-initTabNav();
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
+
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
+tabNav.init();
+
 initModal();
 initTooltip();
 initDropdownMenu();
@@ -23,3 +29,4 @@ initMenuMobile();
 initFuncionamento();
 initFetchAnimais();
 initFetchBitcoin();
+initAnimacaoScroll();
