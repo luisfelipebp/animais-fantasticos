a75bf7d76be437ff3840675f82b6076493f11936
Luis Felipe Barbosa Petrillo <luisfelipebarbosa2013@gmail.com>
2022-07-26 10:38:25 -0300

    Acertado caminho da API

--- js/script.js
+++ js/script.js
@@ -44,6 +44,6 @@ menuMobile.init();
 const funcionamento = new Functionamento("[data-semana]", "aberto");
 funcionamento.init();
 
-fetchAnimais("../../animaisapi.json", ".numeros-grid");
+fetchAnimais("./animaisapi.json", ".numeros-grid");
 
 fetchBitcoin("https://blockchain.info/ticker", ".btc-preco");
